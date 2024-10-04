2f6a6f41a1d78bb99ad5a66345060e7bf0bacbd4
yaredow <yaredyilma11@gmail.com>
2024-10-05 00:18:47 +0300

    finish the signin form component

--- src/components/forms/signin-form.tsx
+++ src/components/forms/signin-form.tsx
@@ -1,8 +1,17 @@
-import { Form, FormField } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { SigninData, SigninSchema } from "@/lib/validation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTransition } from "react";
+import { signin } from "@/app/(auth)/action";
+import { Input } from "../ui/input";
 
 export default function SigninForm() {
   const [error, setError] = useState("");
@@ -16,5 +25,46 @@ export default function SigninForm() {
     },
   });
 
-  return <main>Signin</main>;
+  const onSubmit = (values: SigninData) => {
+    setError("");
+    startTransition(async () => {
+      await signin(values).then((error) => {
+        if (error) setError(error);
+      });
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          name="username"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem className="item-start flex flex-col justify-start">
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" type="text" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="item-start flex flex-col justify-start">
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" type="text" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 }
