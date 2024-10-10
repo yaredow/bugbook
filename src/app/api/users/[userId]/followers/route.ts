d97f7c5982aae7880503375b17532fd8430cced1
yaredow <yaredyilma11@gmail.com>
2024-10-10 16:46:44 +0300

    finish the following status checking route

---
+++ src/app/api/users/[userId]/followers/route.ts
@@ -0,0 +1,51 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { FollowerInfo } from "@/lib/types";
+
+export async function GET(
+  request: Request,
+  { params: { userId } }: { params: { userId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unautherized" }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        followers: {
+          where: {
+            followerId: loggedInUser.id,
+          },
+          select: {
+            followerId: true,
+          },
+        },
+        _count: {
+          select: {
+            followers: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      return Response.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const data: FollowerInfo = {
+      followers: user._count.followers,
+      isFollowedByUser: !!user.followers.length,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
