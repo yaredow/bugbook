926c303ef95cbcb7ee98b63a5f301636477622b0
yaredow <yaredyilma11@gmail.com>
2024-10-11 17:12:13 +0300

    finish optimistic update on the user follow button

--- src/components/follow-button.tsx
+++ src/components/follow-button.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import useGetFollowerInfo from "@/hooks/useGetFollowerInfor";
-import { useMutation } from "@tanstack/react-query";
+import { QueryKey, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button } from "./ui/button";
 import kyInstance from "@/lib/ky";
 import { FollowerInfo } from "@/lib/types";
+import { toast } from "@/hooks/use-toast";
 
 type FollowButtonProps = {
   userId: string;
@@ -16,12 +17,34 @@ export default function FollowButton({
   initialState,
 }: FollowButtonProps) {
   const { data } = useGetFollowerInfo(userId, initialState);
+  const queryClient = useQueryClient();
+  const queryKey: QueryKey = ["follower-info", userId];
 
   const { mutate } = useMutation({
     mutationFn: () =>
       data.isFollowedByUser
         ? kyInstance.delete(`/api/users/${userId}/followers`)
         : kyInstance.post(`/api/users/${userId}/followers`),
+    onMutate: async () => {
+      await queryClient.cancelQueries({ queryKey });
+      const previousState = queryClient.getQueryData<FollowerInfo>(queryKey);
+
+      queryClient.setQueryData<FollowerInfo>(queryKey, () => ({
+        followers:
+          (previousState?.followers || 0) +
+          (previousState?.isFollowedByUser ? -1 : 1),
+        isFollowedByUser: !previousState?.isFollowedByUser,
+      }));
+      return { previousState };
+    },
+    onError: (error, variable, context) => {
+      queryClient.setQueryData(queryKey, context?.previousState);
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again",
+      });
+    },
   });
 
   return (
--- src/hooks/useGetFollowerInfor.ts
+++ src/hooks/useGetFollowerInfor.ts
@@ -2,14 +2,14 @@ import kyInstance from "@/lib/ky";
 import { FollowerInfo } from "@/lib/types";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useGetFollowerInfo(
+export default function useGetFollowerInfor(
   userId: string,
   initialState: FollowerInfo,
 ) {
   const query = useQuery({
     queryKey: ["follower-info", userId],
     queryFn: () =>
-      kyInstance.get(`/api/users/${userId}/followers`).json<FollowerInfo>(),
+      kyInstance.get(`/api/users/${userId}/followers`).json<FollowerInfor>(),
     initialData: initialState,
     staleTime: Infinity,
   });
